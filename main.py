3d4f8dc6c0a6fceeb4292f51b46135bdae37e9e4
Nathan Touchton <ntouchton@protonmail.com>
2023-06-20 15:24:26 -0500

    modified:   main.py 	modified:   templates/index.html 	modified:   templates/login.html

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.secret_key = "xH%o%8TZmfZ!bs7dWz"
 
 class MyForm(FlaskForm):
-    name = StringField("name", validators=[DataRequired()])
+    username = StringField("Email", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
+    # return render_template("login.html")
+    form = MyForm()
     if form.validate_on_submit():
         return redirect('/success')
-    return render_template('submit.html', form=form)
+    return render_template('login.html', form=form)
